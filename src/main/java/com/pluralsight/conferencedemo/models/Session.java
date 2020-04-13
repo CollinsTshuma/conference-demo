d6518f1809ade7dbcb5800b72ca95b3705829743
CollinsTshuma <collinstshuma@gmail.com>
2020-04-13 10:20:12 +0200

    Initial comit:Basic structure and simple shape classes

---
+++ src/main/java/com/pluralsight/conferencedemo/models/Session.java
@@ -0,0 +1,31 @@
+package com.pluralsight.conferencedemo.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity(name="sessions")
+@Getter
+@Setter
+public class Session {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long session_id;
+    private String session_name;
+    private String session_description;
+    private Integer session_Length;
+
+    @ManyToMany
+    @JoinTable(
+            name = "session speakers",
+            joinColumns = @JoinColumn(name = "session_id"),
+            inverseJoinColumns = @JoinColumn(name = "speaker_id")
+    )
+    private List<Speaker> speakers;
+    public  Session(){
+
+    }
+
+}
